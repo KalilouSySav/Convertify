beef7fad0db3a45b5607290e8739d026fab061fe
KalilouSy <savane111@hotmail.fr>
2023-05-19 11:48:10 -0400

    Description des modifications

--- src/components/Historique_component.vue
+++ src/components/Historique_component.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2 class="mb-4"><i class="bi bi-clock"></i> Historique des conversions</h2>
-    <ul class="list-group" style="overflow-y: auto; max-height: 430px; ">
+    <ul class="list-group" style="overflow-y: auto; max-height: 410px; ">
       <li v-for="result in results" :key="result.id" class="list-group-item">
         <div style="padding-right: 5%; padding-left: 5%;">
         <span class="fw-bold">{{ result.value }} {{ result.from }}</span> 
